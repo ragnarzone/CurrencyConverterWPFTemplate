7f96efdd4f28920b9486d0a5d0ab2ea2caf11f76
ragnarzone <ragnarzone@gmail.com>
2024-02-06 10:05:26 +0100

    added functionality for clearing controls and input validations

--- CurrencyConverterWPFTemplate/MainWindow.xaml.cs
+++ CurrencyConverterWPFTemplate/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Windows;
 using System.Windows.Input;
+using System.Text.RegularExpressions;
 
 namespace CurrencyConverterWPFTemplate
 {
@@ -55,16 +56,63 @@ namespace CurrencyConverterWPFTemplate
                 txtCurrency.Focus();
                 return;
             }
+            // else if currency from is not selected or selected default text --SELECT--
+            else if(cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                // show the message
+                MessageBox.Show("Please select currency from", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbFromCurrency.Focus();
+                return;
+            }
+            // else if currency to is not selected or selected default text --SELECT--
+            else if(cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select currency to", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbToCurrency.Focus();
+                return;
+            }
+
+            // check if from and to combobox selected values are same
+            if(cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                // show the label converted currency and converted currency name and ToString("N3") is used to place 000 after the dot(.)
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            } else
+            {
+                ConvertedValue = (double.Parse(cmbFromCurrency.SelectedValue.ToString()) *
+                    double.Parse(txtCurrency.Text)) /
+                    double.Parse(cmbToCurrency.SelectedValue.ToString());
+                   
+                // show the label converted currency and converted currency name
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            lblCurrency.Content = "";
+            ClearControls();
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
 
+        private void ClearControls()
+        {
+            txtCurrency.Text = string.Empty;
+            if(cmbFromCurrency.Items.Count > 0)
+            {
+                cmbFromCurrency.SelectedIndex = 0;
+            }
+            if(cmbToCurrency.Items.Count > 0)
+            {
+                cmbToCurrency.SelectedIndex = 0;
+            }
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
         }
     }
 }
